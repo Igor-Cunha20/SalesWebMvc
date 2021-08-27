85a4dc3f39973a958fb72948307a8ce9bb4666be
Igor Cunha <igorcunha867@gmail.com>
2021-08-27 17:29:30 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,21 +8,31 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ser entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         //[DisplayFormat(DataFormatString = "0:dd/MM/yyyy")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Range(100.00, 50000.0, ErrorMessage = "{0} deve ser de {1} até {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
